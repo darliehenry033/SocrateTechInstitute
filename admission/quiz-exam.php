8a95889a266e7372239b6658a7e84602e3eb949f
JEAN Witch Leyder <jeanwitchleyder@gmail.com>
2025-12-02 22:01:42 +0800

    Start designing the QUIZ system

---
+++ admission/quiz-exam.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . '/../database/database.php';
+require_once __DIR__ . '/../partials/header.php';
+?>
+
+
+<main>
+<section class="quiz-main-container">
+<section class="quiz-header-container">
+     <div class="header-left nav-left">
+             <a href="index.html">
+                <img src="../images/logowhite.png" alt="Socrate Tech Institute"> <!---
+                This will be the student profile picture
+                ----->
+             </a>
+             <h2>JEAN Peter</h2>
+     </div>
+   
+
+     <div class="header-right">
+       <h2><i class="fa-regular fa-clock"></i>05:45</h2>
+      </div>
+
+     </section>
+
+     <section class="question-answers-grid">
+  <div class="question-answer1-grid">Quelle est la valeur de 7 × 8 ?</div>
+  <div class="question-answer2-grid">48</div>
+  <div class="question-answer3-grid">54</div>
+  <div class="question-answer4-grid">56</div>
+  <div class="question-answer5-grid">65</div>
+
+  <div class="question-answer6-grid"><i class="fa-solid fa-arrow-left"></i>Question Précédente</div>
+  <div class="question-answer7-grid">Question Suivante<i class="fa-solid fa-arrow-right"></i></div>
+</section>
+
+
+   
+ 
+
+
+</section>
+</main>
