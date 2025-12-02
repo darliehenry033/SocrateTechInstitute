fb9c97b3672bf81d1374174d0c23b04e6cc21489
JEAN Witch Leyder <jeanwitchleyder@gmail.com>
2025-12-02 15:38:06 +0800

    Designing the page welcome-quiz for redirecting to the real quiz page where will be displaying the question , and options to pick the correct answer

--- admission/quiz.php
+++ admission/quiz.php
@@ -1,11 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Socrate Tech Quiz System</title>
-</head>
-<body>
-    
+<?php
+require_once __DIR__ . '/../database/database.php';
+require_once __DIR__ . '/../partials/header.php';
+?>
+
+
+<main>
+<section class="welcome-main-container">
+<section class="quiz-welcome-header-container">
+     <div class="header-left nav-left">
+             <a href="index.html">
+                <img src="../images/logowhite.png" alt="Socrate Tech Institute">
+             </a>
+     </div>
+     <!-------
+     <div class="header-center">
+           <h1>Plateforme Officielle de Quiz d’Admission</h1>
+     </div>
+-->
+     <div class="header-right">
+       <h2>Session: 2025-2026</h2>
+      </div>
+
+     </section>
+<section class="quiz-welcome-grid-container">
+    <div class="quiz1 quiz-card">
+        <h2>Bienvenue sur la Plateforme Officielle de Quiz d’Admission du Socrate Tech Institute.</h2>
+        <p>Vous êtes invité(e) à compléter cette évaluation afin de finaliser votre candidature pour la session 2025–2026.</p>
+        <p>Ce quiz nous permet d’évaluer votre niveau.</p>
+    </div>
+    <div class="quiz2 quiz-card">
+        <h2>Informations sur le candidat</h2>
+         <ul>
+            <li>Nom:</li>
+            <li>Prénom:</li>
+            <li>Code du candidat:</li>
+            <li>Age:</li>
+         </ul>
+
+    </div>
+    <div class="quiz3 quiz-card">
+    <h2>Règlements du Quiz</h2>
+        <ul>
+           
+            <li>Le quiz comporte plusieurs questions à choix multiples.</li>
+            <li>Une question ne peut pas être modifiée après validation.</li>
+            <li>Aucune aide extérieure n’est autorisée.</li>
+            <li>Veuillez répondre de manière honnête et individuelle.</li>
+            <li>Le temps est limité, restez concentré(e).</li>
+        </ul>
+
+
+    </div>
+    <div class="quiz4 quiz-card">
+        <p>Tout est prêt. Vous pouvez maintenant commencer votre évaluation.</p>
+        
+        <a href="quiz-exam.php"><button class="start-quiz-button button">Commencer</button></a>
+    </div>
+</section>
+<section class="custom-footer quiz-footer">
+<p><i class="fa-regular fa-copyright"></i> 2025 SocrateTech Institute. Tous droits réservés.</p>
+</section>
+
+</section>
+</main>
+
+
+
+
+
+
+
+
 </body>
 </html>
